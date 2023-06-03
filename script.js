895293f1aaeff57e055abf71513f065ec2d2a0f7
Robledo Pazotto <robledo@gmail.com>
2023-06-03 15:15:39 -0700

    v1

---
+++ script.js
@@ -0,0 +1,65 @@
+const gridElement = document.querySelector(".grid");
+const questionElement = document.getElementById("question");
+const inputElement = document.getElementById("input");
+const hintElement = document.getElementById("hint");
+
+let firstNumber;
+let secondNumber;
+let correctAnswer;
+
+for (let row = 0; row <= 12; row++) {
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("row");
+    gridElement.appendChild(rowElement);
+    for (let col = 0; col < 13; col++) {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("cell");
+        if (row === 0 && col === 0) {
+            cellElement.classList.add("grid-header");
+        } else if (row === 0) {
+            cellElement.innerText = `${col}`;
+            cellElement.classList.add("grid-header");
+        } else if (col === 0) {
+            cellElement.innerText = `${row}`;
+            cellElement.classList.add("grid-header");
+        } else {
+            cellElement.classList.add("grid-body");
+            cellElement.dataset.col = col;
+            cellElement.dataset.row = row;
+        }
+        rowElement.appendChild(cellElement);
+    }
+}
+
+function generateNewQuestion() {
+    firstNumber = Math.ceil(Math.random() * 12);
+    secondNumber = Math.ceil(Math.random() * 12);
+    correctAnswer = firstNumber * secondNumber;
+    questionElement.innerText = `What is ${firstNumber} x ${secondNumber}?`;
+}
+generateNewQuestion();
+
+inputElement.focus();
+inputElement.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        hintElement.style.opacity = 0;
+        let answer = Number(inputElement.value);
+        const cell = document.querySelector(
+            `.cell[data-row="${firstNumber}"][data-col="${secondNumber}"]`
+        );
+        cell.classList.remove("wrong");
+        cell.classList.remove("grid-body");
+        if (answer === correctAnswer) {
+            cell.classList.add("correct");
+            cell.innerText = `✅`;
+        } else {
+            cell.classList.add("wrong");
+            cell.innerText = `💥`;
+            hintElement.innerText = `Wrong! ${firstNumber} x ${secondNumber} = ${correctAnswer}`;
+            hintElement.style.opacity = 1;
+        }
+        inputElement.value = "";
+        inputElement.focus();
+        generateNewQuestion();
+    }
+});
